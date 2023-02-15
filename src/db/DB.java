6f200c8e1a9555838dc53d2b83c07ffe08a5bce1
victorsousalima <victorsousa16042005@gmail.com>
2023-02-15 09:58:32 -0300

    Adicionando e configurando o Db

---
+++ src/db/DB.java
@@ -0,0 +1,68 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class DB {
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties props = lerProperties();
+                String url = props.getProperty("dburl");
+                String user = props.getProperty("dbuser");
+                String password = props.getProperty("dbpassword");
+                return DriverManager.getConnection(url, user, password);
+            }
+            catch (SQLException | IOException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    private static Properties lerProperties() throws IOException {
+        Properties props = new Properties();
+        String path = "db.properties";
+        props.load(new FileInputStream(path));
+        return props;
+    }
+
+    public void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            }
+            catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public void closeStatement(Statement st){
+        if (st != null) {
+            try {
+                st.close();
+            }
+            catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+
+    }
+
+    public void closeResultSet(ResultSet rs) throws SQLException {
+        if (rs != null) {
+            try {
+                rs.close();
+            }
+            catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+}
